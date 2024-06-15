65b175b1c0cd1244535b8927acee0e5f52f8b0a2
dawitmanukian <davit.manukyan.d@tumo.org>
2024-06-16 03:48:36 +0400

    update

--- src/App.js
+++ src/App.js
@@ -6,10 +6,29 @@ import LaunchPad from "./pages/launchpad-page/LaunchPad";
 import Footer from "./components/footer/Footer";
 import MobileMenu from "./components/mobile-menu/MobileMenu";
 import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
 
   const [showMobileMenu, setShowMobileMenu] = useState(false)
+  useEffect(() => {
+    const preventHorizontalScroll = (e) => {
+      if (e.touches && e.touches.length === 1) {
+        const touch = e.touches[0];
+        if (touch.clientX < 10 || touch.clientX > window.innerWidth - 10) {
+          e.preventDefault();
+        }
+      }
+    };
+
+    // Adding event listener to prevent default horizontal touchmove behavior
+    document.body.addEventListener('touchmove', preventHorizontalScroll, { passive: false });
+
+    return () => {
+      // Cleanup event listener on component unmount
+      document.body.removeEventListener('touchmove', preventHorizontalScroll);
+    };
+  }, []);
 
   return (
     <>
